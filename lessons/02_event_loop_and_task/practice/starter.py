da68438ac8d13385fe6cf951bda4f9e8c8a88148
nanchengxiao <2844857535@qq.com>
2026-08-09 20:25:04 +0800

    Translate lesson 02 code comments

--- lessons/02_event_loop_and_task/practice/starter.py
+++ lessons/02_event_loop_and_task/practice/starter.py
@@ -1,3 +1,3 @@
 async def build_dashboard(user_id, fetch_user, fetch_orders):
-    # TODO: user and orders are independent I/O and should overlap.
+    # TODO：user 与 orders 彼此无数据依赖，应让两段 I/O 等待尽可能重叠。
     raise NotImplementedError
