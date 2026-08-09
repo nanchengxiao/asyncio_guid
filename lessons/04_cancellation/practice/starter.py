d1650c1e20cf55623d37ad7ab3f9210361929fae
nanchengxiao <2844857535@qq.com>
2026-08-09 20:25:30 +0800

    Translate lesson 04 code comments

--- lessons/04_cancellation/practice/starter.py
+++ lessons/04_cancellation/practice/starter.py
@@ -1,4 +1,4 @@
 async def upload_chunks(chunks, send_chunk, cleanup):
-    # TODO: send all chunks; cleanup must run on success, failure, or caller
-    # cancellation. Do not convert cancellation into a success value.
+    # TODO：发送所有分片；无论成功、普通失败还是调用者取消，cleanup 都必须执行。
+    # 收到取消时不能把它转换成一个“成功”的返回值。
     raise NotImplementedError
