53c65336879a1ffca965aa1413af7027fc4e8418
nanchengxiao <2844857535@qq.com>
2026-08-09 20:25:17 +0800

    Translate lesson 03 code comments

--- lessons/03_structured_concurrency/practice/starter.py
+++ lessons/03_structured_concurrency/practice/starter.py
@@ -1,4 +1,4 @@
 async def run_group(worker_factories):
-    # TODO: one parent scope owns every worker. If one fails, siblings must be
-    # cancelled and allowed to run their cleanup before this function exits.
+    # TODO：让一个父作用域统一拥有所有 worker。任一 worker 失败时，
+    # 其余兄弟 Task 应收到取消请求，并在函数退出前有机会完成自己的清理。
     raise NotImplementedError
