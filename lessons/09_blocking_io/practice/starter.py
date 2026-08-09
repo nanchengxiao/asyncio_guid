7d76ac50cd5b77ab4665db168f2353b1140ebc0d
nanchengxiao <2844857535@qq.com>
2026-08-09 20:26:50 +0800

    Translate lesson 09 code comments

--- lessons/09_blocking_io/practice/starter.py
+++ lessons/09_blocking_io/practice/starter.py
@@ -1,4 +1,4 @@
 async def load_profiles(ids, blocking_loader, *, limit):
-    # TODO: keep the event loop responsive while calling the blocking loader,
-    # and cap concurrent calls into that loader.
+    # TODO：调用同步阻塞 loader 时仍要让 Event Loop 保持响应；同时限制
+    # 工作线程中并发调用 blocking_loader 的数量。
     raise NotImplementedError
