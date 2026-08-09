a769c4491082b21833d1efe1e996bcbe4fa4959f
nanchengxiao <2844857535@qq.com>
2026-08-09 20:27:01 +0800

    Translate lesson 10 code comments

--- lessons/10_business_modeling/practice/starter.py
+++ lessons/10_business_modeling/practice/starter.py
@@ -1,4 +1,4 @@
 async def build_dashboard(user_id, deps):
-    # TODO: implement the DAG described in DESIGN.md and README.
-    # deps exposes: fetch_user, fetch_orders, fetch_account, fetch_recommendations
+    # TODO：按照 DESIGN.md 和 README 中的 DAG 实现业务依赖关系。
+    # deps 提供 fetch_user、fetch_orders、fetch_account、fetch_recommendations。
     raise NotImplementedError
