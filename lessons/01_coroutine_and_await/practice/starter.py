d81103bd2283f0e4fa98cfeb3e9d70e59bc8039a
nanchengxiao <2844857535@qq.com>
2026-08-09 20:24:52 +0800

    Translate lesson 01 code comments

--- lessons/01_coroutine_and_await/practice/starter.py
+++ lessons/01_coroutine_and_await/practice/starter.py
@@ -1,3 +1,3 @@
 async def build_order_context(order_id, fetch_order, fetch_customer):
-    # TODO: fetch order first; customer_id only exists after order returns.
+    # TODO：先获取 order；只有 order 返回后，才能取得 customer_id 并查询 customer。
     raise NotImplementedError
